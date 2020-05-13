76ad0ae64fd687f792ad22b93a307ebefc22e47c
Tanner Black <tannerdblack@gmail.com>
2020-05-13 10:53:08 -0400

    init

---
+++ choco_install_vlc.ps1
@@ -0,0 +1,19 @@
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+
+choco feature enable -n allowGlobalConfirmation
+choco install vlc
+choco install git.install
+choco install sysinternals
+choco install gimp
+choco install filezilla
+choco install python
+choco install sublimetext3
+choco install wireshark
+choco install procmon
+choco install imagemagick.app
+choco install vcredist-all
+choco install jetbrainstoolbox
+choco install intellijidea-ultimate
+choco install clion-ide
